878766c6dba54125b7594e04ffae0e1309217cb9
sanemat <o.gata.ken@gmail.com>
2016-02-01 18:22:34 +0900

    docs(readme): update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// https://developer.mozilla.org/en-US/Add-ons/Firefox_for_Android/Code_snippets#Supporting_both_desktop_and_mobile
 const { Cc, Ci } = require('chrome');
-
+/**
+  * Is firefox android?
+  *
+  * @returns {boolean} true if there is firefox android in firefox addon
+  * @see https://developer.mozilla.org/en-US/Add-ons/Firefox_for_Android/Code_snippets#Supporting_both_desktop_and_mobile
+  */
 module.exports = () =>
 Cc['@mozilla.org/xre/app-info;1']
   .getService(Ci.nsIXULRuntime)
